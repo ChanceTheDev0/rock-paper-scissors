0516d2e04d923e350e9049f22fad922679f5bc64
Chance <111244627+TakingChances01@users.noreply.github.com>
2022-11-20 13:10:50 -0500

    add playRound functon for gameplay

--- js/script.js
+++ js/script.js
@@ -4,21 +4,35 @@ let computerscore = 0;
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random()*3);
     if (computerChoice === 0) {
-        return "rock"
+        return "rock";
     } else if (computerChoice === 1) {
-        return "paper"
+        return "paper";
     } else if (computerChoice === 2) {
-        return "scissors"
+        return "scissors";
     }
 }
-console.log(getComputerChoice())
+
 
 function playRound(player, computer) {
-    if (player === computer){
+    if (player == computer){
         return "draw";
     } else if (player == "rock"){
         if (computer == "paper"){
-            return 
+            return "you lose";
+        } else if (computer = "scissors"){
+            return "you win";
+        };
+    } else if (player == "paper"){
+        if (computer == "scissors"){
+            return "you lose";
+        } else if (computer == "rock"){
+            return "you win";
+        };
+    } else if (player == "scissors"){
+        if (computer == "rock"){
+            return "you lose";
+        } else if (computer == "paper"){
+            return "you win"
         }
     }
 }
