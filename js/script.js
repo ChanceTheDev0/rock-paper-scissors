dd5cd614de728b5c7f0e9bc5fa4950b6a75553fe
Chance <111244627+TakingChances01@users.noreply.github.com>
2022-11-22 16:20:35 -0500

    add for loop for 5 rounds of gameplay

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-let playerscore = 0;
-let computerscore = 0;
-
 function getComputerChoice() {         //cpu player choice function
     let computerChoice = Math.floor(Math.random()*3);
     if (computerChoice === 0) {
@@ -14,31 +11,40 @@ function getComputerChoice() {         //cpu player choice function
 
 
 function playRound(player, computer) {     //round of RPS between player and cpu
-    if (player == computer){
+    if (player === computer) {
         return "draw";
-    } else if (player == "rock"){
-        if (computer == "paper"){
+    } else if (player == "rock") {
+        if (computer == "paper") {
             return "you lose";
-        } else if (computer = "scissors"){
+        } else if (computer = "scissors") {
             return "you win";
         };
-    } else if (player == "paper"){
-        if (computer == "scissors"){
+    } else if (player == "paper") {
+        if (computer == "scissors") {
             return "you lose";
-        } else if (computer == "rock"){
+        } else if (computer == "rock") {
             return "you win";
         };
     } else if (player == "scissors"){
-        if (computer == "rock"){
+        if (computer == "rock") {
             return "you lose";
-        } else if (computer == "paper"){
+        } else if (computer == "paper") {
             return "you win"
         }
     }
 }
 
-
-const answer = (prompt("Enter your choice of rock, paper, or scissors: "));
-
-
+for (i=0; i<5; i++) {
+    let playerSelection = (prompt("Enter your choice of rock, paper, or scissors: "));
+    let computerSelection = getComputerChoice();
+    let playerscore = 0;
+    let computerscore = 0;
+    if (playRound() == "you win") {
+       playerscore + 1;
+    } else if (playRound() == "you lose") {
+       computerscore + 1;
+    } 
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(playerscore, computerscore)
+}
 
